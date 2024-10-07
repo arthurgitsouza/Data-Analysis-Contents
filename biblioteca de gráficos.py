813ba7ba68f7f9e4f23e744347dffb2f5c394744
Joao Arthur Pereira de Souza <joao.souza@DBOSCO.COM.BR>
2024-10-07 14:55:23 -0300

    Class Commit

---
+++ biblioteca de gráficos.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+years = [1950, 1970, 1990, 2010]
+pop = [2.519, 3.692, 5.263, 6.972]
+plt.scatter(years, pop)
+plt.show()
---
+++ histograma.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+x = 0
+y = 0
+help(plt.hist)
